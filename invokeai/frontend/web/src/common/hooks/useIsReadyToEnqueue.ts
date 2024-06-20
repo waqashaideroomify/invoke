e8887bcb5e99335351a42ae18598f5d988f75d17
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2024-06-20 21:16:36 +1000

    refactor(ui): divvy up canvas state a bit

--- invokeai/frontend/web/src/common/hooks/useIsReadyToEnqueue.ts
+++ invokeai/frontend/web/src/common/hooks/useIsReadyToEnqueue.ts
@@ -100,7 +100,7 @@ const createSelector = (templates: Templates) =>
           reasons.push({ content: i18n.t('parameters.invoke.noModelSelected') });
         }
 
-        canvasV2.controlAdapters
+        canvasV2.controlAdapters.entities
           .filter((ca) => ca.isEnabled)
           .forEach((ca, i) => {
             const layerLiteral = i18n.t('controlLayers.layers_one');
@@ -136,7 +136,7 @@ const createSelector = (templates: Templates) =>
             }
           });
 
-        canvasV2.ipAdapters
+        canvasV2.ipAdapters.entities
           .filter((ipa) => ipa.isEnabled)
           .forEach((ipa, i) => {
             const layerLiteral = i18n.t('controlLayers.layers_one');
@@ -164,7 +164,7 @@ const createSelector = (templates: Templates) =>
             }
           });
 
-        canvasV2.regions
+        canvasV2.regions.entities
           .filter((rg) => rg.isEnabled)
           .forEach((rg, i) => {
             const layerLiteral = i18n.t('controlLayers.layers_one');
@@ -200,7 +200,7 @@ const createSelector = (templates: Templates) =>
             }
           });
 
-        canvasV2.layers
+        canvasV2.layers.entities
           .filter((l) => l.isEnabled)
           .forEach((l, i) => {
             const layerLiteral = i18n.t('controlLayers.layers_one');
@@ -208,13 +208,6 @@ const createSelector = (templates: Templates) =>
             const layerType = i18n.t(LAYER_TYPE_TO_TKEY[l.type]);
             const prefix = `${layerLiteral} #${layerNumber} (${layerType})`;
             const problems: string[] = [];
-
-            // if (l.type === 'initial_image_layer') {
-            //   // Must have an image
-            //   if (!l.image) {
-            //     problems.push(i18n.t('parameters.invoke.layer.initialImageNoImageSelected'));
-            //   }
-            // }
 
             if (problems.length) {
               const content = upperFirst(problems.join(', '));
